ea0b83475654cba8ebbeeb5e330c9b234964eb5b
lazina <tambolilazina25@gmail.com>
2026-08-30 00:52:13 +0530

    practice on array,DOM,object,methods etc trough projects

---
+++ color changer/array/script.js
@@ -0,0 +1,43 @@
+let item = document.getElementById("item");
+let button = document.getElementById("add");
+let result=document.getElementById("result");
+let fruits = ["Apple", "Banana", "Mango", "Orange"]
+let list="";
+for (let i = 0; i < fruits.length; i++) {
+list+=fruits[i]+"<br>";
+
+
+}
+result.innerHTML=list;
+// display 
+let student = {
+    name: "Aaysha",
+    age: 20,
+
+    introduce: function() {
+        console.log("Hello, my "+this.name+" is Aaysha and I am"+ this.age +"years old.")
+    }
+};
+student.introduce();
+let products = [
+    {
+        name: "Laptop",
+        price: 50000
+    },
+    {
+        name: "Phone",
+        price: 20000
+    },
+    {
+        name: "Headphones",
+        price: 3000
+    }
+];
+console.log(products[1].name);
+console.log(products[2].price);
+for(int i=1;i<products.length();i++){
+    console.log(products[i].name);
+}
+for(int i=1;i<products.length();i++){
+    console.log(products[i].price);
+}
